b685559644de9168c0d7e3ebe88b2998e3e0c680
Pengfei <44221035+Pengfei196@users.noreply.github.com>
2025-12-03 10:29:27 +0800

    Add files via upload

---
+++ Network_Login.py
@@ -0,0 +1,153 @@
+#-*- coding:utf-8 -*-
+__author__="张云飞，张鹏飞，黎明波"
+import sys
+import requests
+import re
+from datetime import datetime
+
+# 加密函数
+def encrypt(password, secret_key='drcom'):
+    """
+    :param password: 需要加密的原始密码
+    :param secret_key: 自定义密钥（encryption_type='1'时生效）
+    :return: 包含加密结果的字典
+    """
+
+    def getkey(key_str: str) -> int:
+        """生成密钥（基于字符串的异或运算）"""
+        ret = 0
+        for c in key_str:
+            ret ^= ord(c)
+        return ret
+
+    def enc_pwd(pass_in: str, key: int) -> str:
+        """密码加密：异或处理+16进制转换"""
+        if not pass_in:
+            return ""
+        if len(pass_in) > 512:
+            return "-1"
+
+        pass_out = ""
+        for c in pass_in:
+            ch = ord(c) ^ key
+            # 转为两位16进制字符串，大写转小写，补前导零
+            hex_str = hex(ch)[2:].zfill(2)
+            pass_out += hex_str
+        return pass_out
+
+    # 生成密钥
+    key = getkey(secret_key)
+
+    # 加密密码
+    encrypted_pwd = enc_pwd(password, key)
+    return encrypted_pwd
+
+
+# 登录类
+class Login:
+    def __init__(self):
+        pass
+
+    def login(self, url):
+        if self.is_connected()==False:
+            print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} 网络断开，重新连接...")
+            
+            # 获取当前IP地址
+            try:
+                response = requests.get('https://drcom.tyut.edu.cn/', timeout=10)
+                pattern = r"v46ip='([^']*)'"
+                match = re.search(pattern, response.text)
+                if match:
+                    v46ip = match.group(1).strip()
+                    print(f"当前IP地址为: {v46ip}")
+                    url = re.sub(r'wlan_user_ip=[^&]*', f'wlan_user_ip={encrypt(v46ip)}', url)
+                else:
+                    print("获取IP地址失败")
+            except requests.exceptions.RequestException as e:
+                print(f"Request failed: {e}", file=sys.stderr)
+
+            #消息头，Request Header粘过来
+            post_headers={
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Accept-Encoding":"gzip, deflate",
+            "Accept-Language": "zh-CN,zh;q=0.9",
+            "Connection": "keep-alive",
+            "Cookie":"PHPSESSID=iartgtqsnm4i3fg270pkmk2en1",
+            "Host": "219.226.127.250:802",
+            "Referer":"https://drcom.tyut.edu.cn/",
+            "Upgrade-Insecure-Requests":"1",
+            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+            }
+            
+            #数据包，查询字符串参数粘过来
+            post_data={
+            "c": "ACSetting",
+            "a": "Login",
+            "protocol": "http:",
+            "hostname": "219.226.127.250",
+            "port": "",
+            "iTermType":"1",
+            "wlanuserip": "",
+            "wlanacip": "",
+            "wlanacname":"" ,
+            "redirect": "",
+            "session": "",
+            "ssid": "",
+            "vlanid":"" ,
+            "queryACIP":"0",
+            "jsVersion": "1.3.8",
+            "DDDDD": ",0,maxuetao1427",
+            "upass": "DDom3813",
+            "R1": "0",
+            "R2": "",
+            "R6": "0",
+            "v6ip": "",
+            "para": "00",
+            "0MKKey": "123456"
+                }
+            
+            #提交表单
+            try:
+                requests.get(url,headers=post_headers,data=post_data) 
+                if self.is_connected()==True:
+                    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} 连接成功")
+                else:
+                    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} 脚本失效，请重新get抓包，修改--url参数")
+            except Exception as e:
+                print(e)
+                print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} 连接失败，请检查网络")
+
+    #ping百度，确认网络连接状态
+    def is_connected(self):
+        try:
+            test=requests.get("https://www.baidu.com",timeout=5)
+            status=re.search(r'STATUS OK',test.text)
+            if status:
+                return True
+            else:
+                return False
+        except Exception as e:
+            print(e)
+            return False
+
+if __name__=="__main__":
+    """
+    使用说明：
+
+    方法一：
+    ①将学生号，密码分别填写到user_account, user_password变量中
+    ②添加crontab测试任务：* * * * * /usr/bin/python3 ~/cron_test/test_reboot.py >> ~/cron_test/cron.log 2>&1
+
+    方法二：
+    ①在网页端将登录过程的login get url复制下来，赋值给url变量，参考：https://zhuanlan.zhihu.com/p/370801224
+    ②添加crontab测试任务：* * * * * /usr/bin/python3 ~/cron_test/test_reboot.py >> ~/cron_test/cron.log 2>&1
+    """
+    
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} 校园网登陆脚本运行中...")
+    user_account = ""
+    user_password = ""
+    url = f"https://drcom.tyut.edu.cn:802/eportal/portal/login?callback=130546474740&login_method=46&user_account={encrypt(user_account)}&user_password={encrypt(user_password)}&wlan_user_ip=46474659405946434f59464644&wlan_user_ipv6=&wlan_user_mac=474747474747474747474747&wlan_ac_ip=&wlan_ac_name=&mac_type=47&authex_enable=&jsVersion=435944&terminal_type=46&lang=1219&user_agent=3a180d1e1b1b1658425947575f201e191318000457392357464759474c57201e1941434c570f41435e573607071b122012153c1e0358424440594441575f3c3f233a3b5b571b1e1c12573012141c185e57341f05181a12584643455947594759475724161116051e584244405944415732131058464345594759475947&enable_r3=47&encrypt=1&v=1921&lang=en"
+    # url = "https://drcom.tyut.edu.cn:802/eportal/portal/login?callback=130546474740&login_method=46&user_account=4547424e&user_password=0606464430d5959&wlan_user_ip=464746594059464644&wlan_user_ipv6=&wlan_user_mac=474747474747474747474747&wlan_ac_ip=&wlan_ac_name=&mac_type=47&authex_enable=&jsVersion=435944&terminal_type=46&lang=1219&user_agent=3a180d1e1b1b1658425947575f201e191318000457392357464759474c57201e1941434c570f41435e573607071b122012153c1e0358424440594441575f3c3f233a3b5b571b1e1c12573012141c185e57341f05181a12584643455947594759475724161116051e584244405944415732131058464345594759475947&enable_r3=47&encrypt=1&v=1921&lang=en"
+    login=Login()
+    login.login(url)
+
---
+++ README.md
@@ -0,0 +1,26 @@
+# Campus-Network-Authentication-Script   太原理工大校园网自动登陆Python脚本
+---
+
+## 一、校园网登陆脚本
+[Pengfei196/Campus-Network-Authentication-Script: 太原理工大校园网自动登陆Python脚本](https://github.com/Pengfei196/Campus-Network-Authentication-Script)
+#### 原理
+认证过程非常简单，就是将当”学生号”，“密码”和“当前设备分配到的校园网IP”地址通过一个简单的加密函数加密，之后将其与其他信息统一通过一个Request Get方法发送到服务器，服务器比对数据，如果正确则登录成功。
+
+#### 脚本特点
+- 将加密函数写入了脚本，实现了网页登录的完全仿真
+- 自动获取设备IP地址，可自动处理动态IP地址
+- 登录方法二可避免用户密码在脚本中明文出现，保护账号安全（也可以用脚本中的加密函数得到自己账号和密码的结果，替换url中对应的值）
+
+#### 使用方法二图示
+<p align="left"><img src="方法二演示.png" width="100%"/></p>
+
+## 二、Linux定时任务调度
+配合Linux定时任务调度，可以防止24小时开机设备的网络中断
+```bash
+# 编辑crontab
+crontab -e
+
+# 在文件末尾添加以下内容：
+# 每5分钟运行一次，并记录日志
+*/5 * * * * /usr/bin/python3 /home/user/myscript.py >> /home/user/myscript.log 2>&1
+```
